d09b92c50f1e5f1b4a8121b32666ac30010d7534
Vallerga <antoniovallerga85@gmail.com>
2023-05-02 01:58:31 +0200

    minor fix

--- src/main/java/com/design_pattern/controllo_gestione_incendi/model/CentroControlloProxy.java
+++ src/main/java/com/design_pattern/controllo_gestione_incendi/model/CentroControlloProxy.java
@@ -1,7 +1,9 @@
 package com.design_pattern.controllo_gestione_incendi.model;
 
+
 import com.design_pattern.controllo_gestione_incendi.gestione_http.CentroControlloInterface;
 import com.design_pattern.controllo_gestione_incendi.gestione_sonda.AggiornaSonda;
+
 
 public class CentroControlloProxy implements CentroControlloInterface {
 	
